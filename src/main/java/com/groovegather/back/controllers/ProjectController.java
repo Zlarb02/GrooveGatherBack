57e481c16c526864a0443d7b583eeff2a401490f
Etienne Pogoda <pogoda.etienne@outlook.fr>
2024-07-16 20:22:39 +0200

    jwt without google works good

--- src/main/java/com/groovegather/back/controllers/ProjectController.java
+++ src/main/java/com/groovegather/back/controllers/ProjectController.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,7 +22,6 @@ import com.groovegather.back.repositories.ProjectRepo;
 import com.groovegather.back.services.ProjectService;
 
 @RestController
-@CrossOrigin(value = "*")
 @RequestMapping("/api/v1/projects")
 public class ProjectController {
 
